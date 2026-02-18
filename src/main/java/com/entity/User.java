4c7f97e113c1945a5aa3fdafc2b5e4509b7db9d8
PratikGunjal1811 <gunjalpratik1811@gmail.com>
2026-02-18 11:33:14 +0530

    Initial SmartExpense backend setup

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String Name;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+	private String role;
+	
+	
+
+}
---
+++ src/main/java/com/smartexpense/SmartExpenseApplication.java
@@ -0,0 +1,14 @@
+package com.smartexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseApplication.class, args);
+		System.err.println("welcome .....");
+	}
+
+}
